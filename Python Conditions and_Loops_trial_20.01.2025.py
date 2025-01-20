1590ee16a5fbb19cadefffc9697eea6fdc38841c
leylasahverdi <leylasahverdii@gmail.com>
2025-01-20 14:20:20 +0300

    miuul project created

---
+++ Python Conditions and_Loops_trial_20.01.2025.py
@@ -0,0 +1,5 @@
+# 1. Write a function:
+# check_divisibility(num, divisors), which checks if a number is divisible by all the numbers in a given list.
+#
+# If divisible by all, return True; otherwise, return False.
+
