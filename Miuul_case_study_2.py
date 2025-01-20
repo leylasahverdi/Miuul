acd31bd11ee8195971f2d4173b008afebc998d00
leylasahverdi <leylasahverdii@gmail.com>
2025-01-20 20:02:23 +0300

    miuul project functions added for 20-jan

---
+++ Miuul_case_study_2.py
@@ -0,0 +1,16 @@
+# 1.Using the List Comprehension structure, convert the names of the numeric variables in the car_crashes data to uppercase and add NUM
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
+disp=['NUM_'+ i.upper() for i in df.columns]
+
+# 2. Using the List Comprehension structure, write “FLAG” at the end of the names of the variables that do not contain “no” in their name in the car_crashes database.
+
+disp1=[i.upper() + '_FLAG' for i in df.columns if 'no' not in i]
+# 3. Using the List Comprehension structure, select the names of the variables that are DIFFERENT from the variable names given below and create a new dataframe.
+
+og_list = ["abbrev", "no_previous"]
+new_cols=[i for i in df.columns if i not in og_list]
+df=df[new_cols]
